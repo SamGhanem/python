6a6e030f7868486c10fe071018fdadc87a3677e4
SamGhanem <yambo78@outlook.com>
2022-02-13 22:57:52 -0500

    commit

---
+++ user/file.py
@@ -0,0 +1,76 @@
+class User:
+    def __init__(self,username, email):
+        self.username = username
+        self.email = email
+        self.account_balance = 10000
+    
+    def make_withdrawal(self, amount):
+        if (self.account_balance - amount) >= 0:
+            self.account_balance -= amount
+            print(self.account_balance)
+        else:
+            print("YOU DONT HAVE THE MONEY!!!!!")
+        return self
+
+    def make_deposit(self, amount):
+        self.account_balance += amount
+        return self
+    
+    def transfer_money(self,amount,user):
+        if (self.account_balance - amount) >= 0:
+            self.account_balance -= amount
+            user.account_balance += amount
+            print(self.username,self.account_balance)
+            print(user.username,user.account_balance)
+            print(self.username,"Sent", user.username,"$", amount)
+        else:
+            print("YOU DONT HAVE THE MONEY!!!!!")
+        return self
+    
+    def display_user_balance(self):
+        print(f"User: {self.username}, Balance: {self.account_balance}")
+        return self
+
+
+sam = User("Sam G","Sam.g@gmail.com")
+ellie = User("Ellie C","Ellie.c@gmail.com")
+max = User("Max B","Max.b@gmail.com")
+
+print("Sam Account:",sam.account_balance)
+print("Ellie's Account:",ellie.account_balance)
+print("Max's  Account:",max.account_balance)
+print(sam.username,sam.email,sam.account_balance)
+print(ellie.username,ellie.email,ellie.account_balance)
+print(max.username,max.email,max.account_balance)
+
+sam.make_deposit(3000)
+sam.make_withdrawal(100)
+sam.make_withdrawal(75)
+sam.make_withdrawal(90)
+sam.display_user_balance()
+
+print("***************")
+
+ellie.make_deposit(3000)
+ellie.make_deposit(300)
+ellie.make_deposit(300)
+ellie.make_withdrawal(200)
+ellie.display_user_balance()
+
+print("***************")
+
+max.make_deposit(3000)
+max.make_deposit(300)
+max.make_withdrawal(100)
+max.make_withdrawal(6000)
+max.display_user_balance()
+
+print("***************")
+
+# sam.transfer_money(200,ellie)
+# sam.display_user_balance
+# print(sam.account_balance)
+# print("Ellie's Account:",ellie.account_balance)
+sam.transfer_money(50,ellie).display_user_balance()
+max.display_user_balance()
+ellie.display_user_balance()
